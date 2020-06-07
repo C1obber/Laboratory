65507c609f5b1f734eaf998855cf6f547f899eef
C1obber <66168633+C1obber@users.noreply.github.com>
2020-06-07 05:10:01 +0300

    Lab4

--- Lab4/server.js
+++ Lab4/server.js
@@ -1,18 +1,16 @@
-const url = require('url');
-const querystring = require('querystring');
-function start(route, handle) {
-    function onRequest(request, response) { 
-        var pathname = url.parse(request.url).pathname;
-        console.log("Request for " + pathname + " received");
-
-        route(handle, pathname);
-
-        var page = fs.readFileSync('index.html'); 
-        response.writeHead(200, { 'Content-Type': 'text/html' }); 
-        response.write(page);
-        response.end(); 
-    }
-    http.createServer(onRequest).listen(8888); 
-    console.log("Server has started");
+var fs = require("fs");
+var http = require("http");
+function start(route) {
+function onRequest(request, response) {
+var pathname = url.parse(request.url).pathname;
+console.log("Request for" + pathname + "received");
+route(pathname);
+var page = fs.readFileSync('index.html');
+response.writeHead(200, { 'Content-Type': 'text/html' });
+response.write(page);
+response.end();
+}
+http.createServer(onRequest).listen(8888);
+console.log("Server has started");
 }
 exports.start = start;
